a4be6cf6e9387c4e1560ca9129fe0f580be060cb
Kenny Okada <kokada32@gmail.com>
2021-09-02 15:11:26 -0700

    added conditional rendering to the error handling

--- src/index.js
+++ src/index.js
@@ -3,19 +3,30 @@ import ReactDOM from 'react-dom';
 
 class App extends React.Component {
     state = {
-        lat: null
+        lat: null,
+        errorMessage: "",
     }
     
     render() {
         window.navigator.geolocation.getCurrentPosition(
             (position) => {
-                this.setState({ lat: position.coords.latitude })
+                this.setState({ lat: position.coords.latitude });
             },
-            (err) => console.log(err)
+            (err) => {
+                this.setState({ errorMessage: err.message });
+            }
         );
 
-        return <div>Latitude: {this.state.lat}</div>
-    }
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div>Error: {this.state.errorMessage} </div>
+        }
+
+        if (!this.state.errorMessage && this.state.lat) {
+            return <div>Latitude: {this.state.lat} </div>
+        }
+
+        return <div>Loading!</div>;
+    };
 }
 
 ReactDOM.render(
